33876c008c49223231355ecb13107b239d87732f
Rafal Leszczynski <raf.leszcz@gmail.com>
2016-10-09 16:31:30 +0200

    add helper converstion tables

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,4 +1,9 @@
 const Converter = (function () {
+  const romanToArabicNumbers = { I: 1, IV: 4, V: 5, IX: 9, X: 10, XL: 40, L: 50,
+    XC: 90, C: 100, CD: 400, D: 500, CM: 900, M: 1000 };
+  const arabicToRomanNumbers = Object.keys(romanToArabicNumbers)
+    .reduce((obj, key) => Object.assign({}, obj, { [romanToArabicNumbers[key]]: key }), {});
+
   /**
    * @param {*} input
    * @returns {Boolean}
